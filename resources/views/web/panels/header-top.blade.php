175d980a093fc3c6d7f68536f2d66227a0f91974
deepakrawat <deepakrawat@edtechinnovate.com>
2026-04-04 17:49:31 +0530

    update

--- resources/views/web/panels/header-top.blade.php
+++ resources/views/web/panels/header-top.blade.php
@@ -43,7 +43,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/assets-old/images/web-image/web-log.png') }}">
     <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('/assets-old/images/web-image/web-log.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/assets-old/images/web-image/web-log.png') }}">
-    <link  rel="stylesheet" href="{{asset('/assets/css/custom-css.css')}}">
+   
     <link rel="manifest" href="{{ asset('/assets-old/images/web-image/web-log.png') }}">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="{{ asset('/assets-old/images/web-image/web-log.png') }}">
@@ -58,4 +58,4 @@
     <link rel="stylesheet" href="{{asset('/assets/css/magnific-popup.min.css')}}">
     <link rel="stylesheet" href="{{asset('/assets/css/swiper-bundle.min.css')}}">
     <link rel="stylesheet" href="{{asset('/assets/css/style.css')}}">
-    
+     <link  rel="stylesheet" href="{{asset('/assets/css/custom-css.css')}}">
