b8b11d870eed845d270cc37a099254949c1450d7
deepakrawat <deepakrawat@edtechinnovate.com>
2026-04-04 16:51:25 +0530

    update

--- resources/views/web/panels/footer-top.blade.php
+++ resources/views/web/panels/footer-top.blade.php
@@ -787,7 +787,7 @@
    <script src="{{asset('/assets/js/SplitText.min.js')}}"></script>
    <script src="{{asset('/assets/js/lenis.min.js')}}"></script>
    <script src="{{asset('/assets/js/main.js')}}"></script> --}}
-  <script src="{{ asset('assets/js/vendor/jquery.min.js') }}"></script>
+  <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
   <script src="{{ asset('assets/js/swiper-bundle.min.js') }}"></script>
   <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
   <script src="{{ asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
