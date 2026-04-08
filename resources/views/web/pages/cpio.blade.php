34647e24fa7473c278c86d15d98125e8ce83bbb5
rohitraj-ui <rohit@edtechinnovate.com>
2026-04-08 17:05:43 +0530

    new page cpio with responsive in all screens
    new page

--- resources/views/web/pages/cpio.blade.php
+++ resources/views/web/pages/cpio.blade.php
@@ -1,6 +1,7 @@
 @extends('web.layouts.main')
 @section('title', '| CPIO')
-@section('description', 'MIU University is a renowned university, serving students with more than 35 UGC-certified
+@section('description',
+    'MIU University is a renowned university, serving students with more than 35 UGC-certified
     courses.')
 @section('styles')
 @endsection
@@ -74,12 +75,16 @@
                             <h3 class="box-title mb-3">First Appellate Authority (AA)</h3>
                             <div class="academic-meta-wrap d-block">
                                 <div class="academic-meta d-block">
-                                    <p class="mb-0"><i class="fa-solid fa-check"></i> <strong> Name: </strong> Prof. Potsangbam Kumar Singh</p>
-                                    <p><i class="fa-solid fa-check"></i> <strong> Designation: </strong> Pro-Vice Chancellor </p>
+                                    <p class="mb-0"><i class="fa-solid fa-check"></i> <strong> Name: </strong> Prof.
+                                        Potsangbam Kumar Singh</p>
+                                    <p><i class="fa-solid fa-check"></i> <strong> Designation: </strong> Pro-Vice Chancellor
+                                    </p>
                                 </div>
                             </div>
                             <p class="box-text style2 mt-2">
-                                <strong>Responsibility:</strong> Designated to handle appeals and review petitions related to information requests addressed to Manipur International University, Imphal, in accordance with prevailing institutional guidelines.
+                                <strong>Responsibility:</strong> Designated to handle appeals and review petitions related
+                                to information requests addressed to Manipur International University, Imphal, in accordance
+                                with prevailing institutional guidelines.
                             </p>
                         </div>
 
@@ -100,11 +105,12 @@
                             <h3 class="box-title mb-3">Registrar Office</h3>
                             <div class="academic-meta-wrap d-block">
                                 <div class="academic-meta d-block">
-                                    <p class="mb-0"><i class="fa-solid fa-envelope"></i> <strong> Email: </strong> registrar@miu.edu.in</p>
-                                    <p><i class="fa-solid fa-phone"></i> <strong> Mobile: </strong>  +91 9862879287 </p>
+                                    <p class="mb-0"><i class="fa-solid fa-envelope"></i> <strong> Email: </strong>
+                                        registrar@miu.edu.in</p>
+                                    <p><i class="fa-solid fa-phone"></i> <strong> Mobile: </strong> +91 9862879287 </p>
                                 </div>
                             </div>
-                            
+
                         </div>
 
                     </div>
@@ -116,13 +122,100 @@
                             <h3 class="box-title mb-3">Pro-Vice Chancellor Office</h3>
                             <div class="academic-meta-wrap d-block">
                                 <div class="academic-meta d-block">
-                                    <p class="mb-0"><i class="fa-solid fa-envelope"></i> <strong> Email: </strong> provc@miu.edu.in</p>
-                                    <p><i class="fa-solid fa-phone"></i> <strong> Mobile: </strong>  +91 9774011289 </p>
+                                    <p class="mb-0"><i class="fa-solid fa-envelope"></i> <strong> Email: </strong>
+                                        provc@miu.edu.in</p>
+                                    <p><i class="fa-solid fa-phone"></i> <strong> Mobile: </strong> +91 9774011289 </p>
                                 </div>
                             </div>
-                            
+
                         </div>
 
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row mt-5">
+                <div class="col-12">
+                    <div class="t">
+                        <h2 class="sec-title text-anim2 mb-2 mb-md-3"> Postal Address </h2>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="academic-card mb-3 style2 p-5">
+                        <div class="academic-content pb-1 mb-0">
+                            <h3 class="box-title mb-3">
+                                Central Public Information Officer (CPIO) Manipur International University Airport Road,
+                                Ghari, Imphal Imphal West, Manipur – 795140
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row mt-5">
+                <div class="col-12">
+                    <div class="t">
+                        <h2 class="sec-title text-anim2 mb-2 mb-md-3"> Documents Required for RTI Application </h2>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <div class="academic-card mb-3 style2 p-4">
+                        <div class="academic-content pb-1 mb-0">
+                            <h3 class="box-title mb-3"> RTI Application Requirements </h3>
+
+                            <ul>
+                                <li>A clearly written or typed request</li>
+                                <li>Applicant's name and contact details</li>
+                                <li>Specific information sought</li>
+                                <li>Addressed to the concerned Public Information Officer (PIO)</li>
+                                <li>Language: The application can be written in English, Hindi, or the official language of
+                                    the area</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <div class="academic-card mb-3 style2 p-4">
+                        <div class="academic-content pb-1 mb-0">
+                            <h5 class="pb-1"><a href="#">Application Fee: ₹10</a></h5>
+                            <h3 class="box-title mb-3"> Standard fee for each RTI Application payable in favour of Manipur
+                                International University at Imphal via:
+                            </h3>
+
+                            <ul>
+                                <li>Demand Draft</li>
+                                <li>Indian Postal Order</li>
+                                <li>Banker's Cheque</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row mt-5">
+                <div class="col-12">
+                    <div class="t">
+                        <h2 class="sec-title text-anim2 mb-2 mb-md-3"> Declaration under the Right to Information (RTI) Act </h2>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="academic-card mb-3 style2 p-5">
+                        <div class="academic-content pb-1 mb-0">
+                            <p>
+                                The Right of Information Act intends to set out the practical regime of the Right of Information for citizens to enable them to access the information under the control of the public authority in order to promote transparency and accountability in the working of such authority.
+                            </p>
+                            <p>
+                                It is hoped that the information put under the public domain through Manipur International University website would facilitate the citizens to access information through electronic mode, thereby avoiding, to the extent possible, inconvenience to ask formally for such information as may be already open to them.
+                            </h3>
+                            <p>
+                                While compiling the information due care has been exercised, however, if any mistake has crept in due to inadvertence, it is subject to correction.
+                            </p>
+
+                            <p class="fw-bold text-primary"> — Registrar, Manipur International University </p>
+                        </div>
                     </div>
                 </div>
             </div>
